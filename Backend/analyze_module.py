eb52bb5d90ab37e151224c91bd05df3d4abd64a2
Kyrie21323 <kp2733@nyu.edu>
2025-04-17 20:25:33 +0400

    done

--- Backend/analyze_module.py
+++ Backend/analyze_module.py
@@ -263,7 +263,8 @@ class AnalyzeModule:
                         if not overshoot_flag:
                             if 130 <= self.angle_diff(slope_before,slope_now):
                                 overshoot_flag = True
-                                # print("overshoot", data_points[end_position - i])
+                                #for debug
+                                print("overshoot", data_points[end_position - i])
                                 segment['OSP_index'] = (end_position - i)
                                 slope_before = slope_now
                                 i += 25
