8d6bd90748fde6ca07c6f5bcdef221ee7c1f9f3b
Junyong Moon <jm8899@nyu.edu>
2025-05-07 22:19:59 +0400

    Update behavior of DPICalculationModule

--- Backend/calculate_module.py
+++ Backend/calculate_module.py
@@ -3,36 +3,36 @@ import csv
 import math
 
 class DPICalculationModule:
-    def __init__(self, current_profile, current_session, movement_data):
+    def __init__(self, current_profile, movement_data):
         self.__current_profile = current_profile
         self.__movement_data = movement_data
         self.__error = None
         
-        self.__tracking_data = []
+        # self.__tracking_data = []
 
-        current_dir = os.path.dirname(os.path.realpath(__file__))
-        self.__log_file = os.path.join(current_dir, f"id_{current_profile['_id']}_cursor_log_{current_session}.csv")
+        # current_dir = os.path.dirname(os.path.realpath(__file__))
+        # self.__log_file = os.path.join(current_dir, f"id_{current_profile['_id']}_cursor_log_{current_session}.csv")
 
-        try:
-            with open(self.__log_file, "r") as file:
-                reader = csv.reader(file)
-                next(reader) # Skip the first row (header)
+        # try:
+        #     with open(self.__log_file, "r") as file:
+        #         reader = csv.reader(file)
+        #         next(reader) # Skip the first row (header)
 
-                for row in reader:
-                    if len(row) != 4:
-                        continue
+        #         for row in reader:
+        #             if len(row) != 4:
+        #                 continue
 
-                    cursor_log = {}
-                    cursor_log['timestamp'] = row[0]
-                    cursor_log['x'] = int(row[1])
-                    cursor_log['y'] = int(row[2])
-                    cursor_log['clicked'] = row[3]
+        #             cursor_log = {}
+        #             cursor_log['timestamp'] = row[0]
+        #             cursor_log['x'] = int(row[1])
+        #             cursor_log['y'] = int(row[2])
+        #             cursor_log['clicked'] = row[3]
 
-                    self.__tracking_data.append(cursor_log)
+        #             self.__tracking_data.append(cursor_log)
                     
 
-        except Exception as e:
-            self.__error = f"Failed to read tracking data: {str(e)}"
+        # except Exception as e:
+        #     self.__error = f"Failed to read tracking data: {str(e)}"
 
         self.dpi = self.calculate_dpi()
                 
